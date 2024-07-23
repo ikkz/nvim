c9cda904aee4531b816beb1cfa6185bbc3433c73
ikkz <ylei.mk@gmail.com>
2024-07-23 17:28:45 +0800

    update

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,20 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    arg = "lc.nvim",
+    lang = "cpp",
+    cn = {
+      enabled = true,
+    },
+  },
+}
