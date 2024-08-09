3369a915ae0de8fd2bf217d09a1b8d22314e878a
ikkz <ylei.mk@gmail.com>
2024-08-09 23:02:38 +0800

    update

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-require("lualine").setup({
-  options = {
-    theme = "everforest",
-  },
-})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,13 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "everforest",
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+    },
   },
 }
